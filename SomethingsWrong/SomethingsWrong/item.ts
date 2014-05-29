3c13d99074c0f0ab902043dc0ec1ca836386177d
dpk3062 <dpk3062@gmail.com>
2014-05-29 22:50:16 +0200

    Implemented Item and Player

--- SomethingsWrong/SomethingsWrong/item.ts
+++ SomethingsWrong/SomethingsWrong/item.ts
@@ -1,10 +1,33 @@
-// Item
-
-name; 
-
-id;
 
 
+interface IItem {
+    name: string;
+    id: number;
+    weight: number;
+}
 
-// constructor
+//Represents a generic item in the game
+class Item implements IItem {
+    private _name: string;
+    private _id: number;
+    private _weight: number;
 
+    constructor(id: number, name: string, weight: number) {
+        this._id = id;
+        this._name = name;
+        this._weight = weight;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    get weight(): number {
+        return this._weight;
+    }
+
+}
--- SomethingsWrong/SomethingsWrong/player.ts
+++ SomethingsWrong/SomethingsWrong/player.ts
@@ -1,32 +1,51 @@
-class Player
-{
-	var name :String;
-	var inventory : {};
+/// <reference path="inventory.ts" />
 
-	//-- movement states
-	var animationType : enum
-	{
-		IDLE, 
-		WALKLEFT, 
-		WALKRIGHT,
-	}
-	
-	var animationFrame:number = 0;
-	
-	// Draws player at specified screen location.
-	function Draw(screenlocation)
-	{
-		// draw at the screen location whichever animation frame we need or whatever
-		if(this.animationType == 0)
-		{
-			// Draw the idling animation in the appropriate direction
-		}
-		else if(this.animationType == 1)
-		{
-		}
-		else if(this.animationType == 2)
-		{
-		}
-	}
+interface IPlayer {
+    name: string;
+    inventory: IInventory;
+    Draw(x: number, y: number): void;
+}
 
+//-- movement states
+enum AnimationType {
+    IDLE,
+    WALKLEFT,
+    WALKRIGHT
+};
+
+class Player implements IPlayer {
+    private _name: string;
+    private _inventory: IInventory;
+
+    private animaionFrame: number = 0;
+    private animationType: AnimationType = AnimationType.IDLE;
+
+    constructor(name: string) {
+        this._name = name;
+        this._inventory = new Inventory();
+    }
+
+    //Gets the name of this player
+    get name(): string {
+        return this._name;
+    }
+
+    //Gets the inventory of this player
+    get inventory(): IInventory {
+        return this._inventory;
+    }
+
+    //Draws this player at the given screen location
+    Draw(x: number, y: number): void {
+        if (this.animationType === AnimationType.IDLE) {
+            //Draw the idling animation in the appropriate direction
+        } else if (this.animationType === AnimationType.WALKLEFT) {
+            //TODO
+        } else if (this.animationType === AnimationType.WALKRIGHT) {
+            //TODO
+        } else {
+            //TODO handle error
+        }
+
+    }
 }
