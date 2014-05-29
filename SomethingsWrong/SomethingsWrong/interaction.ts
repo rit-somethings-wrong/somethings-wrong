08aecf7ee0897e069af3d368b14fe1ef3519e3c8
dpk3062 <dpk3062@gmail.com>
2014-05-30 00:56:52 +0200

    Stubbed interaction.ts

---
+++ SomethingsWrong/SomethingsWrong/interaction.ts
@@ -0,0 +1,22 @@
+﻿
+
+
+interface IInteraction {
+
+    SetDetails(player: IPlayer, level): void;
+
+    //call the callback function to send results back to the level after the user does something
+    RegisterResult(callback: any): void;
+
+    Clicked(x: number, y: number): void;
+    Enter(): void;
+    Leave(): void;
+
+    Draw(cxt: CanvasRenderingContext2D): void;
+
+}
+
+
+//callback() tells Level results of the interaction, clearInteraction()  -> engine calls Leave()
+//startInteraction(interaction) -> engine calls Enter()
+
