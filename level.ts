e2045d274cac5b29368f09b11c0fea3897f418fb
Gabrielle Bennett <gmb9280@rit.edu>
2014-05-29 19:17:45 +0200

    basic file structure stubs

---
+++ level.ts
@@ -0,0 +1,38 @@
+// Level
+
+// Constructor
+
+
+var id; 
+var img;  // JS image file (background)
+var exits[] // list of ids to levels
+var readyToLeave = false;
+
+// Using the camera info, draws the background at a certain place
+function Draw()
+{ // update and draw }
+function Enter()
+
+// Call to game engine
+
+// What ID are we leaving to?
+// Clean up stuff 
+function Done()
+{
+	// Clean up and then tell engine that we are ready to leave to which other level
+	// It knows where we are and where the exits are / we're going to
+}
+
+function DrawGUI()
+{
+}
+
+// Clicked somewhere in the level - check to see if something is there?
+function Clicked(mx, my)
+{
+}
+
+
+
+
+
