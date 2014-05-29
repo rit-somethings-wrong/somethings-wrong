04a99ebc947fb8796b79af90e375592b9b8724db
dpk3062 <dpk3062@gmail.com>
2014-05-30 00:00:52 +0200

    Fixed IItem -> IStrable bug in Inventory and random code duplication in Player

--- SomethingsWrong/SomethingsWrong/inventory.ts
+++ SomethingsWrong/SomethingsWrong/inventory.ts
@@ -18,30 +18,30 @@ function contains(list, obj): boolean {
 
 
 interface IInventory {
-    GetAllItems(): IItem[];
-    Has(item: IItem): boolean;
-    AddItem(item: IItem): boolean;
-    RemoveItem(item: IItem): boolean;
+    GetAllItems(): IStorable[];
+    Has(item: IStorable): boolean;
+    AddItem(item: IStorable): boolean;
+    RemoveItem(item: IStorable): boolean;
 }
 
 //Manages a weight limited collection of IItems
 class Inventory implements IInventory {
     private static MaxWeight: number = 100;
     private weight: number = 0;  //current weight of the items in our store
-    private items: IItem[] = []; //list of items in our store
+    private items: IStorable[] = [];  //list of items in our store
 
     //Gets all the items stores in this inventory.  If empty, an empty array is returned.
-    GetAllItems(): IItem[] {
+    GetAllItems(): IStorable[] {
         return this.items;
     }
 
     //Returns true if the given item is being stored in this inventory, else returns false.
-    Has(item: IItem): boolean {
+    Has(item: IStorable): boolean {
         return contains(this.items, item);
     }
 
     //Adds the given item to this inventory is there's enough free space (weight) for it.  Returns true if it was added, else false.
-    AddItem(item: IItem): boolean {
+    AddItem(item: IStorable): boolean {
         if (item.weight + this.weight > Inventory.MaxWeight) {
             return false;
         }
@@ -52,7 +52,7 @@ class Inventory implements IInventory {
     }
 
     //Removes all instances of the given item from this inventory.  Returns true if anything was removed, else returns false.
-    RemoveItem(item: IItem): boolean {
+    RemoveItem(item: IStorable): boolean {
         var len = this.items.length;
 
         for (var i = 0; i < this.items.length; i++) {
--- SomethingsWrong/SomethingsWrong/player.ts
+++ SomethingsWrong/SomethingsWrong/player.ts
@@ -1,33 +1,3 @@
-class Player
-{
-	var name :String;
-	var inventory : {};
-
-	//-- movement states
-	var animationType : enum
-	{
-		IDLE, 
-		WALKLEFT, 
-		WALKRIGHT,
-	}
-	
-	var animationFrame:number = 0;
-	
-	// Draws player at specified screen location.
-	function Draw(screenlocation)
-	{
-		// draw at the screen location whichever animation frame we need or whatever
-		if(this.animationType == 0)
-		{
-			// Draw the idling animation in the appropriate direction
-		}
-		else if(this.animationType == 1)
-		{
-		}
-		else if(this.animationType == 2)
-		{
-		}
-	}
 /// <reference path="inventory.ts" />
 
 interface IPlayer extends IEntity {
@@ -60,7 +30,7 @@ class Player extends Entity implements IPlayer {
     //Draws this player at the given screen location
     Draw(location: Vector): void {
         if (this.animationType === AnimationType.IDLE) {
-            //TODO daw the idling animation in the appropriate direction
+            //TODO draw the idling animation in the appropriate direction
         } else if (this.animationType === AnimationType.WALKLEFT) {
             //TODO
         } else if (this.animationType === AnimationType.WALKRIGHT) {
