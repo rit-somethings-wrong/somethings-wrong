b855f9f55754572e28d818a4920984dceefd0997
Martin Turski <martin.turski@gmx.de>
2014-05-30 17:57:52 +0200

    added pause menu GUI panel

--- SomethingsWrong/SomethingsWrong/gui_interface.ts
+++ SomethingsWrong/SomethingsWrong/gui_interface.ts
@@ -1,25 +1,17 @@
+/// <reference path="collision.ts" />
 /// <reference path="engine.ts" />
 // gui_interface.ts - classes for drawing and managing the GUI layer of our game.
 
-function loadImage( imageIdentifier : string )
+class GUIPanel implements IUIHandler
 {
-    var loadedImage = new Image();
-    
-    loadedImage.src = imageIdentifier;
-    
-    return loadedImage;
-}
-
-class GUIPanel
-{
-    private ourEngine : GameEngine;
-    private bgImage : any;
-    private children : any;
-    private position : Vector;
-    private width : number;
-    private height : number;
-    private imageWidth : number;
-    private imageHeight : number;
+    public ourEngine : GameEngine; // engine pointer
+    private bgImage : any;          // background image of this panel
+    private children : any;         // idea: children GUI panel that render afterward?
+    private position : Vector;      // position of the GUI panel on the canvas
+    private width : number;         // drawing width
+    private height : number;        // drawing height
+    private imageWidth: number;     // loaded image width (null if picture not loaded)
+    private imageHeight : number;   // loaded image height (null if picture not loaded)
     
     constructor( theEngine : GameEngine )
     {
@@ -33,12 +25,16 @@ class GUIPanel
     LoadBackgroundImage( filePath : string ) : any
     {
         var image = new Image();
-        var guiPanelObject = this;
+        var thisPtr = this;
+
+        // Reset the image dimensions.
+        this.imageWidth = null;
+        this.imageHeight = null;
         
         image.onload = function()
         {
-            guiPanelObject.imageWidth = this.width;
-            guiPanelObject.imageHeight = this.height;
+            thisPtr.imageWidth = this.width;
+            thisPtr.imageHeight = this.height;
             
             image.onload = null;
         }
@@ -46,6 +42,23 @@ class GUIPanel
         image.src = filePath;
         
         return image;
+    }
+
+    getBackgroundImageWidth(): number {
+        return this.imageWidth;
+    }
+
+    getBackgroundImageHeight(): number {
+        return this.imageHeight;
+    }
+
+    transformMouseToImageCoordinates(mx: number, my: number): Vector {
+        // Get 0..1 scalars based on GUI panel plane.
+        mx /= this.width;
+        my /= this.height;
+
+        // Return a vector that is in the image space.
+        return new Vector(mx * this.imageWidth, my * this.imageHeight);
     }
     
     Draw( context : any ) : void
@@ -62,27 +75,83 @@ class GUIPanel
     }
     
     // GUIPanel can receive clicking events.
-    Clicked(mx : number, my : number)
+    Clicked(mx : number, my : number) : boolean
     {
         // this method is meant to be overridden.
+
+        // by default, GUIPanels accepts clicks.
+        return true;
+    }
+
+    Typed(charString: string) : boolean {
+        return false;
     }
 };
 
 class GUIInventoryScreen extends GUIPanel
 {
+    private testButtonArea: BoundingRectangle;  // mouse intersection area in image space (pixels)
+
     constructor( theEngine : GameEngine )
     {
         super( theEngine );
 
         this.LoadBackgroundImage(
             IMAGES.inventoryImg
-        );
+            );
+
+        this.testButtonArea = new BoundingRectangle(new Vector(50, 50), 100, 60);
     }
-    
-    Clicked(mx : number, my : number)
+
+    Clicked(mx: number, my: number) : boolean
     {
+        // Test: try to click on the given area. if done, output some debug to console.
+        var mouseCoordVector = this.transformMouseToImageCoordinates(mx, my);
+
+        if (this.testButtonArea.intersectWithPoint(mouseCoordVector)) {
+            console.log("clicked successfully!");
+        }
         
-        
-        super.Clicked( mx, my );
+        return super.Clicked(mx, my);
     }
 };
+
+class GUIPauseScreen extends GUIPanel {
+    private saveQuitArea: BoundingRectangle;
+    private continueArea: BoundingRectangle;
+
+    constructor(theEngine: GameEngine) {
+        super(theEngine);
+
+        // TODO: set up the rectangles properly.
+        this.saveQuitArea = new BoundingRectangle(new Vector(30, 30), 100, 40);
+        this.continueArea = new BoundingRectangle(new Vector(30, 150), 100, 40);
+    }
+
+    Clicked(mx: number, my: number): boolean {
+        // Get the coordinate in image space.
+        var backgroundClickAt = this.transformMouseToImageCoordinates(mx, my);
+
+        var hasClickedButton = false;
+
+        // Either save/quit.
+        if (this.saveQuitArea.intersectWithPoint(backgroundClickAt)) {
+            // we clicked at save/quit button.
+
+            hasClickedButton = true;
+        }
+
+        // Or continue.
+        if (this.continueArea.intersectWithPoint(backgroundClickAt)) {
+            // we clicked at continue button.
+
+            hasClickedButton = true;
+        }
+
+        if (hasClickedButton) {
+            this.ourEngine.ClearConversation();
+        }
+
+        return true;
+    }
+}
