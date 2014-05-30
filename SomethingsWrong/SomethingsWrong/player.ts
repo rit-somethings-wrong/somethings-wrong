23b5ccd137f0b5e294df8c3ba18ea09bc35173c8
Gabrielle Bennett <gmb9280@rit.edu>
2014-05-30 10:24:28 +0200

    loading in testchar, updated player.ts (not sure if i can run that...)

--- SomethingsWrong/SomethingsWrong/player.ts
+++ SomethingsWrong/SomethingsWrong/player.ts
@@ -11,14 +11,26 @@ enum AnimationType {
     WALKRIGHT
 };
 
+
+
 class Player extends Entity implements IPlayer {
     private _inventory: IInventory;
 
     private animaionFrame: number = 0;
+	private animationCounter: number = 0;
+	private animationCounterMax: number = 10; // so that animation doesn't move too fast
+	
     private animationType: AnimationType = AnimationType.IDLE;
+	
+	private spriteWidth : number = 23; // for testchar
+	private spriteHeight : number = 44; // for testchar 
+	
+	private spriteImg : Image;
 
-    constructor(id: number, name: string) {
+    constructor(id: number, name: string, imgUrl : string) {
         super(id, name);
+		this.spriteImg = new Image(); 
+		this.spriteImg.src = imgUrl;
         this._inventory = new Inventory();
     }
 
@@ -31,6 +43,8 @@ class Player extends Entity implements IPlayer {
     Draw(location: Vector): void {
         if (this.animationType === AnimationType.IDLE) {
             //TODO draw the idling animation in the appropriate direction
+			
+			
         } else if (this.animationType === AnimationType.WALKLEFT) {
             //TODO
         } else if (this.animationType === AnimationType.WALKRIGHT) {
@@ -38,6 +52,14 @@ class Player extends Entity implements IPlayer {
         } else {
             //TODO handle error
         }
-
+		
+		/* Increment animationCounter, which will trigger a change of animationFrame upon reaching animationCounterMax */
+		animationCounter++;
+		if(this.animationCounter > this.animationCounterMax){ 
+			// Increment animation frame and reset counter
+			this.animationFrame++; if(this.animationFrame >= 3){ this.animationFrame = 0; } // assumes everything has 4 frames of animation
+			this.animationCounter = 0; 
+		}
+		
     }
 }
