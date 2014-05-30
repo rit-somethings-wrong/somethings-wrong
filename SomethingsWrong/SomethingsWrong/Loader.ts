05ef41287b3a156f8041dfeef5e3be610838924e
dpk3062 <dpk3062@gmail.com>
2014-05-30 17:53:05 +0200

    Removed Loader.js and adjusted whitespace

--- SomethingsWrong/SomethingsWrong/Loader.ts
+++ SomethingsWrong/SomethingsWrong/Loader.ts
@@ -2,11 +2,11 @@
 
 // CONSTANTS
 var KEYBOARD = {
-	"KEY_LEFT": 37, 
-	"KEY_UP": 38, 
-	"KEY_RIGHT": 39, 
-	"KEY_DOWN": 40,
-	"KEY_SPACE": 32
+    "KEY_LEFT": 37,
+    "KEY_UP": 38,
+    "KEY_RIGHT": 39,
+    "KEY_DOWN": 40,
+    "KEY_SPACE": 32
 };
 
 var IMAGES = 
@@ -27,29 +27,29 @@ var IMAGES =
 // load everything
 declare var Modernizr: any;
 Modernizr.load(
-	{
-		load: 
-		[
-			// core code files
-			'createjs.js',
-			'SoundLoader.js',
-			'interfaces.js',
-			'collision.js',
-			'utilities.js',
-			'entity.js',
-			'item.js',
-			'inventory.js',
-			'player.js',
-			'interaction.js',
-			'level.js',
-			'levelscene.js',
+    {
+        load:
+        [
+            // core code files
+            'createjs.js',
+            'SoundLoader.js',
+            'interfaces.js',
+            'collision.js',
+            'utilities.js',
+            'entity.js',
+            'item.js',
+            'inventory.js',
+            'player.js',
+            'interaction.js',
+            'level.js',
+            'levelscene.js',
 
-			// level definitions - must be loaded after the core code files
-			'./levels/airport/airport.js',
+            // level definitions - must be loaded after the core code files
+            './levels/airport/airport.js',
 
-			// game start - must be loaded last
-			'engine.js',
-			
+            // game start - must be loaded last
+            'engine.js',
+            
             IMAGES.playerImg,
             // general scenes.
             IMAGES.inventoryImg,
@@ -60,48 +60,48 @@ Modernizr.load(
             IMAGES.busStationBackImg,
             IMAGES.jennysHouseOutsideBackImg,
             IMAGES.toolshedBackImg
-			// also load other class files here
-		],
-			
-			// on complete
-		complete: function()
-		{
-			
-			console.log("Loading complete with Modernizr.");
-			// and other stuff you might need upon everything having been loaded
-			window.onblur = function()
-			{
-				//game.paused = true;
-				//game.keydown = []; // clear key daemon
-				//game.update(); // show pause screen
-			};
-			
-			window.onfocus = function()
-			{
-				//game.paused = false;
-				//game.update(); // resume game play
-			};
-			
-			//------- event listeners
-			
-			window.addEventListener("keydown",function(e){
-				console.log("keydown=" + e.keyCode);
-				//game.keydown[e.keyCode] = true;
-			});
-				
-			window.addEventListener("keyup",function(e){
-				console.log("keyup=" + e.keyCode);
-				//game.keydown[e.keyCode] = false;
-			});
-			
-			
-			// init game
-			//game.keydown = []; // clear key demon
-			//game.init(); // init game
-			
-		},
-		
-		
-	
-	}
+            // also load other class files here
+        ],
+            
+            // on complete
+        complete: function()
+        {
+            
+            console.log("Loading complete with Modernizr.");
+            // and other stuff you might need upon everything having been loaded
+            window.onblur = function()
+            {
+                //game.paused = true;
+                //game.keydown = []; // clear key daemon
+                //game.update(); // show pause screen
+            };
+            
+            window.onfocus = function()
+            {
+                //game.paused = false;
+                //game.update(); // resume game play
+            };
+            
+            //------- event listeners
+            
+            window.addEventListener("keydown",function(e){
+                console.log("keydown=" + e.keyCode);
+                //game.keydown[e.keyCode] = true;
+            });
+                
+            window.addEventListener("keyup",function(e){
+                console.log("keyup=" + e.keyCode);
+                //game.keydown[e.keyCode] = false;
+            });
+            
+            
+            // init game
+            //game.keydown = []; // clear key demon
+            //game.init(); // init game
+            
+        },
+        
+        
+    
+    }
 );
